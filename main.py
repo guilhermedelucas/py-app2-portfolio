ef8cab9ee1cdb350e2488b22c774d01d5b441e1c
guilhermedelucas <gdelucasde@gmail.com>
2023-07-11 17:32:06 +0200

    Add image, description and links

--- main.py
+++ main.py
@@ -15,24 +15,33 @@ with col2:
     My development career starts in 2016, when I studied at Spiced Academy, and on 
     2017 started my first developer job at Dots. Now I am looking to expand my 
     career and learn about Machine Learning and AI"""
-    st.write(content)
+    st.info(content)
 
 
 content2 = """Bellow you can find some of the apps I have built in Python. Feel free to contact me!"""
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, emptycol, col4 = st.columns([1.5, 0.5, 1.5])
 
 
 df = pandas.read_csv('data.csv', sep=";")
+
+
+def display_app(row_local):
+    st.header(row_local['title'])
+    st.write(row_local['description'])
+    st.image(f"images/{row_local['image']}")
+    st.write(f"[Source_code]({row_local['url']})")
+
+
 with col3:
     for index, row in df[:10].iterrows():
-        st.header(row['title'])
+        display_app(row)
 
 
 with col4:
     for index, row in df[10:].iterrows():
-        st.header(row['title'])
+        display_app(row)
 
 
 # with col4:
