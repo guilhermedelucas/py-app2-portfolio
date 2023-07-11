08936fca754377b92520a076e661e7bd17c66574
guilhermedelucas <gdelucasde@gmail.com>
2023-07-11 18:49:50 +0200

    Remove password

--- send_email.py
+++ send_email.py
@@ -1,24 +1,16 @@
-import smtplib
-import ssl
-
-host = "smtp.gmail.com"
-port = 465
-
-username = "gdelucasde@gmail.com"
-password = "epyontzyxzwfnuyx"
-
-receiver = "gdelucasde@gmail.com"
-
-context = ssl.create_default_context()
+import smtplib, ssl
 
 
-message = """\
-Subject: Hi!
-How are you?
-Bye!
+def send_email(email, topic, message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "gdelucasde@gmail.com"
+    receiver = "gdelucasde@gmail.com"
+    context = ssl.create_default_context()
+    message = f"""\
+Subject: {topic}!
+{message}
 """
-
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(email, receiver, message)
