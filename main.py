dfa3352c81677ad1378df24c96180c5d7a15b9b9
guilhermedelucas <gdelucasde@gmail.com>
2023-07-11 05:47:13 +0200

    Images, created title and description

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+
+with col2:
+    st.title('Guilherme de Lucas')
+    content = """Hi, I am Guilherme! I am a javascript developer, learning Python. 
+    My development career starts in 2016, when I studied at Spiced Academy, and on 
+    2017 started my first developer job at Dots. Now I am looking to expand my 
+    career and learn about Machine Learning and AI"""
+    st.write(content)
